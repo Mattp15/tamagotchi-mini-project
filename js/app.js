1b24a221e0e7c5fbe7132e4a5a071e74b6f87c3c
Mattp15 <Matthewrphillips15@gmail.com>
2022-07-05 23:19:01 -0400

    added js variables for images and text displays

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ const darkMode = document.querySelector('#dark');
 const feed = document.querySelector('#feed');
 const play = document.querySelector('#play');
 const sleep = document.querySelector('#sleep');
+
+//text displays
+const rules = document.querySelector('article');
+const playerTitle = document.querySelector('#player-title');
+const playerName = document.querySelector('#player-name');
+const hungerStat = document.querySelector('li #hunger-stat');
+const sleepStat = document.querySelector('li #sleep-stat');
+const boredomStat = document.querySelector('li #bordeom-stat');
+const ageStat = document.querySelector('li #age-stat');
+//images and gifs
+let egg = document.querySelector('#egg');
+let baby = document.querySelector('#baby');
+let child = document.querySelector('#child');
+let adult = document.querySelector('#adult');
+let eating = document.querySelector('#eating-gif');
+let playing = document.querySelector('#playing-gif');
+let scared = document.querySelector('#scared-gif');
+//button functions
+
+//function on timeintervol that checks for death?
