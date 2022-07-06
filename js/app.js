9f95f31cb2c1c18a98af9ba198321f2a3c77ef88
Mattp15 <Matthewrphillips15@gmail.com>
2022-07-05 23:11:56 -0400

    starting js, added rules

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//buttons
+const submit = document.querySelector('#submit');
+const darkMode = document.querySelector('#dark');
+const feed = document.querySelector('#feed');
+const play = document.querySelector('#play');
+const sleep = document.querySelector('#sleep');
